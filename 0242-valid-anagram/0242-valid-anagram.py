570137886022274375d8e48bd76b7efec251fade
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-09-08 15:20:06 +0530

    Time: 50 ms (51.68%), Space: 16.9 MB (47.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s=list(s)
-        t=list(t)
-        s=sorted(s)
-        t=sorted(t)
-        if len(s)!=len(t):
-            return False
-        else:
-            for i in range(len(s)):
-                if s[i]!=t[i]:
-                    return False
+        t_hash={}
+        for i in t:
+            if i in t_hash:
+                t_hash[i]+=1
+            else:
+                t_hash[i]=1
+        for i in s:
+            if i in t_hash:
+                t_hash[i]-=1
+            else:
+                return False
+        for i in t_hash:
+            if t_hash[i]!=0:
+                return False
         return True
