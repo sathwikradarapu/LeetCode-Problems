e16c4269aa80289e097145336a553ebeee86d641
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-09-12 22:35:59 +0530

    Time: 56 ms (53.85%), Space: 16.6 MB (12.57%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -1,10 +1,27 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        res = []
-        prev_word = ""
-        for word in words:
-            sorted_word = "".join(sorted(word))
-            if sorted_word != prev_word:
-                res.append(word)
-            prev_word = sorted_word
+        hash_map={}
+        res=[]
+        for i in range(len(words)):
+            if i==0:
+                a=" ".join(sorted(words[i]))
+                hash_map[a]=[words[i]]
+                for i in hash_map.values():
+                    res.extend(i)
+                hash_map={}
+            else:
+                a=" ".join(sorted(words[i]))
+                b=" ".join(sorted(words[i-1]))
+                if a!=b:
+                    if a in hash_map:
+                        hash_map[a].append(words[i])
+                        for i in hash_map.values():
+                            res.extend(i)
+                        hash_map={}
+                    else:
+                        hash_map[a]=[words[i]]
+                        for i in hash_map.values():
+                            res.extend(i)
+                        hash_map={}
         return res
+        
