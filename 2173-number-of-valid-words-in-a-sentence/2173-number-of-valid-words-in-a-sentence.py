1fe22575d4999861c1c7ad7767771af1903e40a2
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-09-13 14:01:10 +0530

    Time: 49 ms (34.07%), Space: 16.6 MB (58.01%) - LeetHub

--- 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.py
+++ 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.py
@@ -1,25 +1,64 @@
 class Solution:
     def countValidWords(self, sentence: str) -> int:
-        sentence_list = sentence.split()
-        count = 0
-        for i, word in enumerate(sentence_list):
-            valid = True
-            hyphen_count = 0  
-            for j, letter in enumerate(word):
-                if letter.isdigit():
-                    valid = False 
+        # time 51 ms, faster than 85.11% effiecent, space inefficent 16.7 MB, less than 14.18%
+        alphabets = 'abcdefghijklmnopqrstuvwxyz'
+        digits = '0123456789'
+        punctuations = '!.,'
+        hyphens = '-'
+        
+        # remove any spaces at the beginning or end of the sentence if there any.
+        
+        tokens= []
+        word = ''
+        # make an array of tokens to make that 
+        for i in sentence:
+            if i != ' ':
+                word+=i
+            else:
+                if word != '':
+                    tokens.append(word)
+                word=''
+        if word != '':
+            tokens.append(word) #appends the last word in the senctence
+        
+        print(tokens)
+        
+        validWords = 0
+        
+        
+        for word in tokens:
+            hyphenCounter = 0
+            y =  word[:len(word)-1]
+            #There is at most one punctuation mark. If present, it must be at the end of the token
+            if '!' in y or '.' in y or ',' in y:
+                continue
+            # if haphen come at the begining or end continue
+            if word[0] == '-' or word[-1] =='-':
+                continue
+                
+            # test the validity of each charcter separately 
+            for i in range(len(word)):
+                if word[i] in digits:   # if there is a digit break
                     break
-                if letter in ("!",".",",") and j != len(word)-1:
-                    valid = False 
-                    break
-                if (letter == "-" and (j == len(word)-1 or j == 0)) or (letter == "-" and (not word[j-1].isalpha() or not word[j+1].isalpha())):
-                    valid = False 
-                    break
-                if letter == "-":
-                    hyphen_count +=1
-                    if hyphen_count >1:
-                        valid = False 
+                    
+                
+                if word[i] == '-':
+                    # If present, it must be surrounded by lowercase characters
+                    if word[i-1] not in alphabets or word[i+1] not in alphabets:
                         break
-            if valid:
-                count +=1
-        return count
+                    hyphenCounter +=1
+                
+                    
+                # There is at most one hyphen '-'
+                if hyphenCounter >1:
+                    break
+                    
+                # if you have reached the end of word increament validwords  
+                if i == len(word)-1:
+                    validWords +=1
+                    
+                
+            
+        
+        
+        return validWords
