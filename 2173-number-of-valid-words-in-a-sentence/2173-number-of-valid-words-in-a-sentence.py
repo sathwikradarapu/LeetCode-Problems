328157b6d80bb44a0b123ac4bf098f7baa9bf84a
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-09-13 14:00:01 +0530

    Time: 39 ms (85.82%), Space: 16.7 MB (58.01%) - LeetHub

---
+++ 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countValidWords(self, sentence: str) -> int:
+        sentence_list = sentence.split()
+        count = 0
+        for i, word in enumerate(sentence_list):
+            valid = True
+            hyphen_count = 0  
+            for j, letter in enumerate(word):
+                if letter.isdigit():
+                    valid = False 
+                    break
+                if letter in ("!",".",",") and j != len(word)-1:
+                    valid = False 
+                    break
+                if (letter == "-" and (j == len(word)-1 or j == 0)) or (letter == "-" and (not word[j-1].isalpha() or not word[j+1].isalpha())):
+                    valid = False 
+                    break
+                if letter == "-":
+                    hyphen_count +=1
+                    if hyphen_count >1:
+                        valid = False 
+                        break
+            if valid:
+                count +=1
+        return count
