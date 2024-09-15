fd5ac7629755ad34caa3d35ab4ac91bfcff88e9a
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-09-15 14:03:39 +0530

    Time: 94 ms (29.76%), Space: 21.4 MB (34.72%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash_map = {}
+        for i in range(len(nums)):
+            if nums[i] in hash_map:
+                hash_map[nums[i]] += 1
+            else:
+                hash_map[nums[i]] = 1
+
+        # Sort the hash_map by values in descending order
+        sorted_hash_map = dict(sorted(hash_map.items(), key=lambda item: item[1], reverse=True))
+        ans = list(sorted_hash_map.keys())[:k]
+        return ans
+        
