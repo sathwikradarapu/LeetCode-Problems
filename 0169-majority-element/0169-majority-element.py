9b1b0e9d4e6adadde1b553bb594e01fcf524fe26
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-10-05 11:31:15 +0530

    Time: 163 ms (87.82%), Space: 18 MB (83.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash_map={}
-        for i in nums:
-            if i in hash_map:
-                hash_map[i]+=1
-            else:
-                hash_map[i]=1
+        nums=sorted(nums)
         test_case=len(nums)//2
-        for i in hash_map:
-            if hash_map[i]>test_case:
-                return i
+        return nums[test_case]
                 
