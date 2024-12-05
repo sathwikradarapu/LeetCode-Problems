651f81b1d56e3a933671a596f709035c6df4cc05
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-12-05 10:58:16 +0530

    Time: 87 ms (68.18%), Space: 32.4 MB (5.03%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.py
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        left = 0
-        bitmask = 0  # Tracks the OR of the current subarray
-        max_length = 0
+        
 
-        for right in range(len(nums)):
-            while (bitmask & nums[right]) != 0:  # If new element violates "nice" condition
-                bitmask ^= nums[left]  # Remove nums[left] from current bitmask
-                left += 1  # Shrink window
-            
-            bitmask |= nums[right]  # Add nums[right] to the current bitmask
-            max_length = max(max_length, right - left + 1)  # Update max length
-
-        return max_length
+        l=0
+        n=len(nums)
+        bit_mask=0
+        ans=float("-inf")
+        for r in range(n):
+            while(bit_mask&nums[r]!=0):
+                bit_mask^=nums[l]
+                l+=1
+            bit_mask|=nums[r]
+            ans=max(ans,r-l+1)
+        return ans
