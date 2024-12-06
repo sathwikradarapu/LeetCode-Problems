28866d5e8b226d3b8b3f5afd39952b55cbdb1e52
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-12-06 11:32:52 +0530

    Time: 7 ms (93.81%), Space: 17.4 MB (12.62%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -7,20 +7,14 @@ class Solution:
         window_count = {}
 
         for r in range(n):
-            # Add current element to the window
             if nums[r] not in window_count:
-                window_count[nums[r]] = 0
-            window_count[nums[r]] += 1
-
-            # Shrink the window from the left while it has all distinct elements
+                window_count[nums[r]] = 1
+            else:
+                window_count[nums[r]] += 1
             while len(window_count) == total_distinct:
-                # Count subarrays from l to r and beyond
-                count += n - r
-
-                # Shrink the window
                 window_count[nums[l]] -= 1
                 if window_count[nums[l]] == 0:
                     del window_count[nums[l]]
                 l += 1
-
+                count += n-r
         return count
