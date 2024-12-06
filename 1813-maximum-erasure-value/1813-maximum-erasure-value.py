f103b4ac57e92853a3f775ce78331af547517b4b
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-12-06 10:26:14 +0530

    Time: 7955 ms (5.07%), Space: 28.5 MB (56.43%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -2,16 +2,16 @@ class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         l=0
         n=len(nums)
-        ans=0
-        uniset=set()
+        hash_set=set()
+        ans=float("-inf")
         for r in range(n):
-            ch=nums[r]
-            if ch not in uniset:
-                uniset.add(ch)
+            num=nums[r]
+            if num not in hash_set:
+                hash_set.add(num)
             else:
-                while ch in uniset:
-                    uniset.remove(nums[l])
+                while num in hash_set:
+                    hash_set.remove(nums[l])
                     l+=1
-                uniset.add(ch)
-            ans=max(ans,sum(uniset))
+                hash_set.add(num)
+            ans=max(ans,sum(hash_set))
         return ans
