fa4e7876005f22f4e78dc36609849fb9f618ed74
Radarapu_Sathwik2933 <98262085+sathwikradarapu@users.noreply.github.com>
2024-12-08 10:50:45 +0530

    Time: 0 ms (100%), Space: 17.5 MB (7.32%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,24 +2,19 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Merges nums2 into nums1 in-place, assuming nums1 has sufficient space at the end for nums2.
-        """
-        # Start from the end of both arrays
-        i, j, k = m - 1, n - 1, m + n - 1
-        
-        # Merge nums1 and nums2 in reverse order
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-        
-        # Copy remaining elements from nums2 (if any)
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+        i=m-1
+        j=n-1
+        k=m+n-1
+        while i>=0 and j>=0:
+            if nums1[i]>=nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
+            elif nums1[i]<nums2[j]:
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
+        while j>=0:
+            nums1[k]=nums2[j]
+            j-=1
+            k-=1
+        return nums1
