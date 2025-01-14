826e86e7a24b8cd8a3da4e5919df20644b2bad15
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-01-14 14:42:12 +0530

    Time: 17 ms (84.47%), Space: 19.9 MB (15.48%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,20 +1,10 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        # Sort the greed factors and cookie sizes
         g.sort()
         s.sort()
-        
-        # Initialize pointers for g and s
-        i = 0
-        j = 0
-        
-        # Use the two pointers to find the maximum number of content children
-        while i < len(g) and j < len(s):
-            if s[j] >= g[i]:
-                # The cookie is big enough to satisfy the child
-                i += 1  # Move to the next child
-            # Move to the next cookie
-            j += 1
-        
-        # The number of content children is i
+        i=j=0
+        while i<len(g) and j<len(s):
+            if s[j]>=g[i]:
+                i+=1
+            j+=1
         return i
