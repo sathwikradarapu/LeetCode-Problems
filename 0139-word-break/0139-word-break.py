684c8e2bdb8e542bd81c0096fcb981b0674846b7
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-01-18 14:06:43 +0530

    Time: 3 ms (69.98%), Space: 17.8 MB (50.44%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,17 +2,12 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True  # Base case for empty string
-        
-        for i in range(len(s) - 1, -1, -1):
-            for word in wordDict:
-                # Check if the word fits in the substring starting at i
-                if i + len(word) <= len(s) and s[i:i+len(word)] == word:
-                    dp[i] = dp[i + len(word)]
-                if dp[i]:  # If dp[i] becomes True, no need to check further
+        dp=[False]*(len(s)+1)
+        dp[len(s)]=True
+        for i in range(len(s)-1,-1,-1):
+            for j in wordDict:
+                if ((i+len(j))<=len(s)) and s[i:i+len(j)]==j:
+                    dp[i]=dp[i+len(j)]
+                if dp[i]:
                     break
-        
         return dp[0]
-
-
