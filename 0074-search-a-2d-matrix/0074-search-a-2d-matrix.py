07990eb249c7e2d7cbd6d82ee115e41243a4542c
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-01-21 11:25:49 +0530

    Time: 0 ms (100%), Space: 18.3 MB (15.75%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        r = len(matrix)
+        c = len(matrix[0])
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j] == target:
+                    return True
+        return False
