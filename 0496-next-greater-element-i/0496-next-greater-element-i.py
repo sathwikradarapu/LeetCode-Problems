d7801e614991c9fd3644bc1780b434f1ba1bfacf
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-01 14:34:38 +0530

    Time: 2 ms (48.43%), Space: 17.9 MB (52.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,21 +1,15 @@
-from typing import List
-
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # Dictionary to store the next greater element for each number in nums2
-        next_greater = {}
-        stack = []
-        
-        # Process nums2 using a monotonic decreasing stack
-        for num in nums2:
-            while stack and stack[-1] < num:
-                smaller = stack.pop()
-                next_greater[smaller] = num  # Map the smaller element to its next greater element
-            stack.append(num)
-        
-        # Remaining elements in stack have no greater element
-        while stack:
-            next_greater[stack.pop()] = -1
-        
-        # Prepare the result for nums1
-        return [next_greater[num] for num in nums1]
+        stack=[]
+        dici={}
+        res=[]
+        for i in nums2:
+            while stack and stack[-1]<i:
+                j=stack.pop()
+                dici[j]=i
+            stack.append(i)
+        for i in stack:
+            dici[i]=-1
+        for i in nums1:
+            res.append(dici[i])
+        return res
