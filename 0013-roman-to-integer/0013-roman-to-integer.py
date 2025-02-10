46d9b6b8af79e271d43a79a546101cb773e9dfb6
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-10 14:56:31 +0530

    Time: 3 ms (80.69%), Space: 17.9 MB (37.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,15 +4,13 @@ class Solution:
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
         }
-        total = 0
-        n = len(s)
-        
+        n=len(s)
+        total=0
         for i in range(n):
-            current_value = roman_values[s[i]]
-            # Check if the next value exists and is greater
-            if i + 1 < n and current_value < roman_values[s[i + 1]]:
-                total -= current_value
+            j=roman_values[s[i]]
+            if i+1<n and j<roman_values[s[i+1]]:
+                total-=j
             else:
-                total += current_value
-        
+                total+=j
         return total
+
