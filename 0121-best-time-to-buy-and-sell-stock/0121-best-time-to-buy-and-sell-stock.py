66dbc0bb4e0b2d28da829d9af2a994b5efb7a077
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-11 10:50:14 +0530

    Time: 134 ms (22.52%), Space: 27 MB (31.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minval=prices[0]
-        ans=0
-        for i in range(len(prices)):
-            minval=min(prices[i],minval)
-            ans=max(ans,(prices[i]-minval))
+        n=len(prices)
+        mini=float("inf")
+        ans=float("-inf")
+        for i in range(n):
+            mini=min(prices[i],mini)
+            ans=max(ans,(prices[i]-mini))
         return ans
