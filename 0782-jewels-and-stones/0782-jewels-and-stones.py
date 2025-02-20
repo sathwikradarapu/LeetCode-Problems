2eface69a11bc7865c4735e3f3d1eeaac7ac3ed3
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-20 14:39:40 +0530

    Time: 0 ms (100%), Space: 17.4 MB (99.58%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,11 +1,7 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        hash_map={}
+        c=0
         for i in stones:
             if i in jewels:
-                if i in hash_map:
-                    hash_map[i]+=1
-                else:
-                    hash_map[i]=1
-        return sum(hash_map.values())
-        
+                c+=1
+        return c
