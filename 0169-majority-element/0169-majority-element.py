dae976fce5a05ad8a9ae7bfb933c2f41a30c70eb
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 14:29:54 +0530

    Time: 7 ms (57.08%), Space: 19.4 MB (19.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash_map={}
+        dici={}
         for i in nums:
-            if i in hash_map:
-                hash_map[i]+=1
+            if i in dici:
+                dici[i]+=1
             else:
-                hash_map[i]=1
-        test_case=len(nums)//2
-        for i in hash_map:
-            if hash_map[i]>test_case:
-                return i
-                
+                dici[i]=1
+        ans=max(list(dici.values()))
+        for num,freq in dici.items():
+            if freq==ans:
+                return num
