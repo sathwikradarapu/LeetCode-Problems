c21716856794c90da5a2b502cec5478b0231c69d
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 14:15:48 +0530

    Time: 3 ms (83.1%), Space: 18 MB (24.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,33 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row={i:set() for i in range(9)}
-        column={i:set() for i in range(9)}
+        col={i:set() for i in range(9)}
         box={i:set() for i in range(9)}
-
-        for i in range(9):
-            for j in range(9):
-                ele=board[i][j]
-                if ele!='.':
-                    if ele in row[i]:
-                        return False
-                    else:
-                        row[i].add(ele)
-        for j in range(9):
-            for i in range(9):
-                ele=board[i][j]
-                if ele!='.':
-                    if ele in column[j]:
-                        return False
-                    else:
-                        column[j].add(ele)
         for i in range(9):
             for j in range(9):
                 ele=board[i][j]
                 if ele!='.':
                     index=3*(i//3)+(j//3)
-                    if ele in box[index]:
+                    if ele in row[i] or ele in col[j] or ele in box[index]:
                         return False
-                    else:
-                        box[index].add(ele)
+                    row[i].add(ele)
+                    col[j].add(ele)
+                    box[index].add(ele)
         return True
-            
