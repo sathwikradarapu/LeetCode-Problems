834cbccf2116c88f6fe2e0e1e1bb6c6fd6b4461d
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 16:35:52 +0530

    Time: 1 ms (32.54%), Space: 17.9 MB (50.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        a=['(','[','{']
-        b=[')',']','}']
-        c=[]
+        dici={
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        stack=[]
         for i in s:
-            if i in a:
-                c.append(i)
+            if i not in dici:
+                stack.append(i)
             else:
-                if len(c)!=0:
-                    if (i==b[0] and c[-1]==a[0]) or (i==b[1] and c[-1]==a[1]) or (i==b[2] and c[-1]==a[2]):
-                        c.pop()
-                    else:
-                        return False
-                else:
+                if not stack or stack[-1]!=dici[i]:
                     return False
-        if len(c)==0:
+                else:
+                    stack.pop()
+        if not stack:
             return True
-        else:
-            return False
+        return False
