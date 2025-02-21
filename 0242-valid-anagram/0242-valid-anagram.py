a66a3aad7027e8fa0da5ec40b9c62e7b1b30cccd
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 09:53:44 +0530

    Time: 11 ms (73.18%), Space: 17.7 MB (86.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s=sorted(s)
-        t=sorted(t)
-        return s==t
+        dici1=dict()
+        dici2=dict()
+        for i in s:
+            if i not in dici1:
+                dici1[i]=1
+            else:
+                dici1[i]+=1
+        for j in t:
+            if j not in dici2:
+                dici2[j]=1
+            else:
+                dici2[j]+=1
+        return(dici1==dici2)
