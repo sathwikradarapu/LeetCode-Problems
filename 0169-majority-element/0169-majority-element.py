788ce2fb894f172ec60190f691ee804b96715c5d
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 14:30:54 +0530

    Time: 11 ms (30.48%), Space: 19.4 MB (52.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        cond=(n//2)
         dici={}
         for i in nums:
             if i in dici:
                 dici[i]+=1
             else:
                 dici[i]=1
-        ans=max(list(dici.values()))
         for num,freq in dici.items():
-            if freq==ans:
+            if freq>cond:
                 return num
