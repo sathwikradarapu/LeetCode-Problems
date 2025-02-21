9797e58e1c02f9b7eb28ef4162fb4c703fd45135
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 14:24:17 +0530

    Time: 12 ms (62.4%), Space: 20.5 MB (95.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,10 @@
-from typing import List
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash_map = {}
-        for word in strs:
-            # Sort the word and use it as a key
-            sorted_word = "".join(sorted(word))
-            if sorted_word in hash_map:
-                hash_map[sorted_word].append(word)
+        dici={}
+        for i in strs:
+            j="".join(sorted(i))
+            if j in dici:
+                dici[j].append(i)
             else:
-                hash_map[sorted_word] = [word]
-        
-        # Convert dict_values to a list
-        return list(hash_map.values())
+                dici[j]=[i]
+        return list(dici.values())
