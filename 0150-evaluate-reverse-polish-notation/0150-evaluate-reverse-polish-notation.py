6b3da4f018d2b3d3c9bc5180808b24a03af1d657
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-02-21 16:56:14 +0530

    Time: 3 ms (65.33%), Space: 19.3 MB (17.52%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,25 +1,19 @@
-from typing import List
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        
-        for token in tokens:
-            if token.isnumeric() or (token.startswith('-') and token[1:].isnumeric()):
-                # Handle both positive and negative numbers
-                stack.append(int(token))
+        stack=[]
+        for i in tokens:
+            if i.isdigit() or (i.startswith("-") and i[1:].isdigit()):
+                stack.append(int(i))
             else:
-                a = stack.pop()  # Second operand
-                b = stack.pop()  # First operand
-                
-                if token == '+':
-                    stack.append(b + a)
-                elif token == '-':
-                    stack.append(b - a)
-                elif token == '*':
-                    stack.append(b * a)
-                elif token == '/':
-                    # Ensure truncation towards zero
-                    stack.append(int(b / a))
-        
-        return stack[0]  # Final result will be the only element in stack
+                b=stack.pop()
+                a=stack.pop()
+                if i=='+':
+                    c=b+a
+                elif i=='-':
+                    c=a-b
+                elif i=='*':
+                    c=b*a
+                else:
+                    c=int(a/b)
+                stack.append(c)
+        return stack[-1]
