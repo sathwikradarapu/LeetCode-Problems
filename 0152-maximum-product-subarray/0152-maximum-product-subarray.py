201aa0b893808fbd6c7faeea5217bb360baf74ff
Sathwik Radarapu <98262085+sathwikradarapu@users.noreply.github.com>
2025-04-20 10:53:57 +0530

    Time: 11 ms (52.24%), Space: 18.4 MB (37.83%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -5,8 +5,10 @@ class Solution:
         length=len(nums)
         max_product=float("-inf")
         for index in range(length):
+            #if 0 occurs then considering new sub array
             if(prefix==0):
                 prefix=1
+            #if 0 occurs then considering new sub array
             if(suffix==0):
                 suffix=1
             prefix*=nums[index]
