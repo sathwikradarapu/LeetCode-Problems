7fe3f7905a7e069629a50ba28a5e07a29d27bc96
Sathwik Radarapu <sathwikradarapu@gmail.com>
2025-07-30 07:53:37 +0530

    Time: 18 ms (9.43%), Space: 32.2 MB (5.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def palindrome(s, l, r):
+        def palindrome(s,l,r):
             nonlocal ans
-            
-            if l >= r:
+            if l>=r:
                 return
             else:
                 if not s[l].isalnum():
-                    palindrome(s, l + 1, r)
+                    palindrome(s,l+1,r)
                 elif not s[r].isalnum():
-                    palindrome(s, l, r - 1)
+                    palindrome(s,l,r-1)
                 else:
-                    if s[l].lower() == s[r].lower():
-                        palindrome(s, l + 1, r - 1)
+                    if s[l].lower()==s[r].lower():
+                        palindrome(s,l+1,r-1)
                     else:
-                        ans = False
+                        ans=False
                         return
-        l = 0
-        r = len(s) - 1
-        ans = True
-        palindrome(s, l, r)
+        l=0
+        r=len(s)-1
+        ans=True
+        palindrome(s,l,r)
         return ans
