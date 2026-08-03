27bc5790540d492e83e8d9e3cef16732139da155
Sathwik Radarapu <sathwikradarapu@gmail.com>
2026-08-03 16:10:04 +0530

    Time: 641 ms (53.78%), Space: 22.2 MB (81.49%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,26 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        n = len(nums)
-        ans = []
-        for left in range(n - 2):
-            if left > 0 and nums[left] == nums[left - 1]:
-                continue  # skip duplicate 'left' values
-            middle, right = left + 1, n - 1
-            while middle < right:
-                total = nums[left] + nums[middle] + nums[right]
-                if total == 0:
-                    ans.append([nums[left], nums[middle], nums[right]])
-                    middle += 1
-                    right -= 1
-                    # skip duplicates
-                    while middle < right and nums[middle] == nums[middle - 1]:
-                        middle += 1
-                    while middle < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif total > 0:
-                    right -= 1
-                else:
-                    middle += 1
-        return ans
+        answer=[]
+        length=len(nums)
+        for left in range(length-2):
+            if left>0 and nums[left]==nums[left-1]:
+                continue
+            else:
+                middle=left+1
+                right=length-1
+                while middle<right:
+                    total=nums[left]+nums[middle]+nums[right]
+                    if total==0:
+                        answer.append([nums[left],nums[middle],nums[right]])
+                        while middle<right and nums[middle]==nums[middle+1]:
+                            middle+=1
+                        while middle<right and nums[right]==nums[right-1]:
+                            right-=1
+                        middle+=1
+                        right-=1
+                    elif total>0:
+                        right-=1
+                    else:
+                        middle+=1
+        return answer
